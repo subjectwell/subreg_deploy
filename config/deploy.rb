c25f135d59fc22fabcd319bea76fb9fe7b3b67cb
Tim <tim@brightfire.net>
2018-11-02 14:21:25 -0500

    Updated the puma restart to use sudo.

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,7 +25,7 @@ namespace :deploy do
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
-      execute "/usr/local/etc/rc.d/puma restart"
+      execute "sudo /usr/local/etc/rc.d/puma restart"
     end
   end
 
